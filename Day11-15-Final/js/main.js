3acb4bb6b0b0b30a7e945a351b6d56b8670d6545
sptungG <ngoviettung154@gmail.com>
2021-01-29 17:43:59 +0700

    update user profile

--- Day11-15-Final/js/main.js
+++ Day11-15-Final/js/main.js
@@ -19,7 +19,26 @@ toggle between hiding and showing the dropdown content */
 //       }
 //     }
 //   }
+/*======================== SLIDE ========================*/
+var slideIndex = 0;
+showSlides();
 
+function showSlides() {
+   var i;
+   var slides = document.getElementsByClassName("mySlides");
+   var dots = document.getElementsByClassName("dot");
+   for (i = 0; i < slides.length; i++) {
+   slides[i].style.display = "none";  
+   }
+   slideIndex++;
+   if (slideIndex > slides.length) {slideIndex = 1}    
+   for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex-1].style.display = "block";  
+   dots[slideIndex-1].className += " active";
+   setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
 /*======================== TOP button ========================*/
 var mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
