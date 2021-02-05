e3b7f4b892c1b2eafae5872989cda2bd84263635
sptungG <ngoviettung154@gmail.com>
2021-02-05 14:39:35 +0700

    update filter&search

--- test-FDish/main.js
+++ test-FDish/main.js
@@ -1,4 +1,4 @@
-/*======================== GRID ========================*/
+
 function resizeGridItem(item){
     grid = document.getElementsByClassName("grid")[0];
     rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
@@ -26,13 +26,22 @@ function resizeGridItem(item){
   for(x=0;x<allItems.length;x++){
     imagesLoaded( allItems[x], resizeInstance);
   }
-
 /*======================== TOP button ========================*/
-/*======================== DropDown Menu ========================*/
-/*======================== Popup Blog ========================*/
-function openForm() {
-  document.getElementById("myBlog").style.display = "block";
-}
-function closeForm() {
-  document.getElementById("myBlog").style.display = "none";
-}
+//Get the button:
+  mybutton = document.getElementById("myBtn");       
+// When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+        
+        function scrollFunction() {
+          if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+            mybutton.style.display = "block";
+          } else {
+            mybutton.style.display = "none";
+          }
+        }
+        
+// When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+          document.body.scrollTop = 0; // For Safari
+          document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
